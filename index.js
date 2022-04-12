b72466d13900e7af9b05f9bef1276070b5d24786
Sergio <kurisergio777777@gmail.com>
2022-04-12 13:09:52 -0400

    commit tree 87ef5ec7154b2da64e47d3fd9f05f15a75240904

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your code here!
+
+let rm_main = document.querySelector("#main");
+rm_main.remove();
+
+
+let newHeader = document.createElement("h1");
+newHeader= `victory`
+document.body.append(newHeader)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let element = document.createElement(`div`);
+
+
+Document.body.append(element);
+
+const ul = document.createElement("ul");
+
+
+
+for (i=0;i<3;i++){
+    let liv = document.createElement(`li`);
+    liv.innerText = (i + 1).toString();
+    ul.append(liv);
+    
+}
+
+element.append(ul)*/
+
